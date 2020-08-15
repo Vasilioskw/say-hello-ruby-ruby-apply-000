173c3f77afa2374db170e197e42ac07a22b2e620
Vasilios Kambouras <vasilioskw@gmail.com>
2020-08-15 02:10:59 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,15 @@
+<<<<<<< HEAD
 def say_hello(name = "Ruby Programmer")
   puts "Hello #{name}!"
+=======
+say_hello (
+do "Hello " + "Ruby Programmer" + "!")
+"Ruby Programmer" = ["Kent Beck", ""]
+"" = "Ruby Programmer"
+
+if Ruby Programmer === "Kent Beck" 
+  expect "Hello " + "Kent Beck" + "!"
+  if Ruby Programmer === "" 
+  expect "Hello " + "Ruby Programmer" + "!"
+>>>>>>> bb2fdbf96cec3a497b7781d194ce4089550330cf
 end
